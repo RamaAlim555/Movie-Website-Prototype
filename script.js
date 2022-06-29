8d242bda75cde30d3b752d7f548af1d519bee606
RamaAlim555 <ramaalim34@gmail.com>
2022-06-29 13:35:52 +0700

    Next
    nothing

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ var sInput;
 
 function sMovie(judul)
 {
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c623d88' +judul)
-    .then(Response => console.log(Response))
+    fetch('https://www.omdbapi.com/?i=tt3896198&apikey=c623d88&s=' +judul)
+    .then(response => Response.json())
+    .then(response => console.log(Response.json()))
+    
 }
 
 function getSearchValue()
 {
     sInput = document.getElementById("searchBox").value
     document.getElementById("searchTittle").innerHTML = 'Search : "'+ sInput  + '"';
-    sInput = ""
     sMovie(sInput);
+    sInput = ""
 }
